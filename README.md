21c385b0f7fc7bb840c7708575d57cd861797e31
Nijia Lin <louis70109@gmail.com>
2024-03-30 13:17:17 +0800

    ✨ init project

---
+++ README.md
@@ -0,0 +1,34 @@
+# FastAPI 佈署到 Cloud Run 範例
+
+本範例展示了如何使用 Google Cloud Run 快速佈署 FastAPI 應用程式。這個範例將引導您完成以下步驟：
+
+- 使用 Dockerfile 建立一個 Docker 映像檔。
+- 使用 gcloud CLI 在 Cloud Run 上啟動映像檔與佈署。
+- 測試在 Cloud Run 上執行的 FastAPI 應用程式。
+
+## 前置要求
+
+在開始之前，您需要具備以下要求：
+
+- 一個 Google Cloud Platform 帳戶
+- 安裝了 gcloud CLI 工具
+- 已經安裝 Docker 在本地端環境中
+- Firebase's realtime DB URL
+- Gemini API Key
+
+## 使用方式
+
+[![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
+
+
+```
+git clone https://github.com/louis70109/linebot-gemini-fastapi-example.git
+
+cd linebot-gemini-fastapi-example/
+
+gcloud run deploy my-fastapi-example --source .
+```
+
+## 授權
+
+MIT
---
+++ main.py
@@ -0,0 +1,124 @@
+import logging
+import os
+import sys
+if os.getenv('API_ENV') != 'production':
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+
+from fastapi import FastAPI, HTTPException, Request
+
+from linebot.v3.webhook import WebhookParser
+from linebot.v3.messaging import (
+    AsyncApiClient,
+    AsyncMessagingApi,
+    Configuration,
+    ReplyMessageRequest,
+    TextMessage)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent,
+)
+
+import uvicorn
+from firebase import firebase
+import google.generativeai as genai
+
+logging.basicConfig(level=os.getenv('LOG', 'WARNING'))
+logger = logging.getLogger(__file__)
+
+app = FastAPI()
+
+channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
+channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+if channel_secret is None:
+    print('Specify LINE_CHANNEL_SECRET as environment variable.')
+    sys.exit(1)
+if channel_access_token is None:
+    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+    sys.exit(1)
+
+configuration = Configuration(
+    access_token=channel_access_token
+)
+
+async_api_client = AsyncApiClient(configuration)
+line_bot_api = AsyncMessagingApi(async_api_client)
+parser = WebhookParser(channel_secret)
+
+
+firebase_url = os.getenv('FIREBASE_URL')
+gemini_key = os.getenv('GEMINI_API_KEY')
+
+
+# Initialize the Gemini Pro API
+genai.configure(api_key=gemini_key)
+
+
+@app.get("/health")
+async def health():
+    return 'ok'
+
+
+@app.post("/webhooks/line")
+async def handle_callback(request: Request):
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = await request.body()
+    body = body.decode()
+
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Invalid signature")
+
+    for event in events:
+        logging.info(event)
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessageContent):
+            continue
+        text = event.message.text
+        user_id = event.source.user_id
+        msg_type = event.message.type
+        fdb = firebase.FirebaseApplication(firebase_url, None)
+        user_chat_path = f'chat/{user_id}'
+        chat_state_path = f'state/{user_id}'
+        chatgpt = fdb.get(user_chat_path, None)
+
+        if msg_type == 'text':
+
+            if chatgpt is None:
+                messages = []
+            else:
+                messages = chatgpt
+
+            if text == '!清空':
+                text = TextMessage(text='對話歷史紀錄已經清空！')
+                fdb.delete(user_chat_path, None)
+            else:
+                model = genai.GenerativeModel('gemini-pro')
+                messages.append({'role': 'user', 'parts': [text]})
+                response = model.generate_content(messages)
+                messages.append({'role': 'model', 'parts': [response.text]})
+                # 更新firebase中的對話紀錄
+                fdb.put_async(user_chat_path, None, messages)
+        await line_bot_api.reply_message(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=response.text)]
+            )
+        )
+    return 'OK'
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', default=8000))
+    debug = True if os.environ.get(
+        'API_ENV', default='develop') == 'develop' else False
+    logging.info('Application will start...')
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=debug)
